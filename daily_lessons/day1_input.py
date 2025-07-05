886b3871e4b39442a90bb0eeb56bf9581bbdd4b9
Reyu <reyuyt00@gmail.com>
2025-07-05 17:23:46 +0545

    Add Day 1 input/output script

---
+++ daily_lessons/day1_input.py
@@ -0,0 +1,9 @@
+# Day 1: Basic Input and Output
+
+name = input("What is your name? ")
+print("Hello, " + name + "! Welcome to Python on mobile.")
+
+age = int(input("How old are you? "))
+print("In 5 years, you will be", age + 5)
+
+
